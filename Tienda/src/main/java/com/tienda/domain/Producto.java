d8a1ff50cfde0c88eb1f1d7178b86c5d3148b4da
Alejandro1103 <113400144+Alejandro1103@users.noreply.github.com>
2024-08-16 00:04:54 -0600

    Commit 5

--- Tienda/src/main/java/com/tienda/domain/Producto.java
+++ Tienda/src/main/java/com/tienda/domain/Producto.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tienda.domain;
 
-/**
- *
- * @author Alejandro
- */
-public class Producto {
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="producto")
+public class Producto implements Serializable {
     
+    private static final long serialVersionUID = 1l;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long idProducto;
+    //private Long idCategoria; no se usa mas por el @ManyToOne
+    private String descripcion;
+    private String rutaImagen;
+    private String detalle;
+    private double precio;
+    private int existencias;
+    private boolean activo;  
+    
+    @ManyToOne
+    @JoinColumn(name="id_categoria")
+    private Categoria categoria;
 }
