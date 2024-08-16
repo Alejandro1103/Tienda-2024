69f4d85dce738640eadc05fdc97d411333d5887c
Alejandro1103 <113400144+Alejandro1103@users.noreply.github.com>
2024-08-16 00:25:15 -0600

    Commit 9

--- Tienda/src/main/java/com/tienda/services/impl/ProductoServicesImpl.java
+++ Tienda/src/main/java/com/tienda/services/impl/ProductoServicesImpl.java
@@ -1,13 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tienda.services.impl;
 
-/**
- *
- * @author Alejandro
- */
-public class ProductoServicesImpl {
+import com.tienda.dao.ProductoDao;
+import com.tienda.domain.Producto;
+import com.tienda.services.ProductoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class ProductoServicesImpl implements ProductoService {
+
+    @Autowired
+    private ProductoDao productoDao;
+
+    @Override
+    public List<Producto> getProductos(boolean activos) {
+        var lista = productoDao.findAll();
+
+        if (activos) {
+            //Solo quiero las categoreias activas...
+            lista.removeIf(c -> !c.isActivo());
+        }
+
+        return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Producto getProducto(Producto producto) {
+        return productoDao.findById(producto.getIdProducto()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Producto producto) {
+        productoDao.save(producto);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Producto producto) {
+        productoDao.delete(producto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> consulta1 (double precioInf, double precioSup){
+        return productoDao.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
+    }
     
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> consulta2 (double precioInf, double precioSup){
+        return productoDao.consultaJPQL(precioInf, precioSup);
+    }
+    
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> consulta3 (double precioInf, double precioSup){
+        return productoDao.consultaSQL(precioInf, precioSup);
+    }
+
 }
--- Tienda/src/main/java/com/tienda/services/impl/UsuarioDetailsServiceImpl.java
+++ Tienda/src/main/java/com/tienda/services/impl/UsuarioDetailsServiceImpl.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tienda.services.impl;
 
-/**
- *
- * @author Alejandro
- */
-public class UsuarioDetailsServiceImpl {
+import com.tienda.dao.UsuarioDao;
+import com.tienda.domain.Rol;
+import com.tienda.domain.Usuario;
+import com.tienda.services.UsuarioDetailsService;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("userDetailsService")
+public class UsuarioDetailsServiceImpl implements UsuarioDetailsService, UserDetailsService{
+    
+    @Autowired
+    private UsuarioDao usuarioDao;
+    @Autowired
+    private HttpSession session;
+    
+    
+    @Override
+    @Transactional(readOnly=true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+        //se busca el registro en la tabla usuario que tenga el username pasado por prm
+        Usuario usuario = usuarioDao.findByUsername(username);
+        
+        //se valida si el usuario se encontro
+        if(usuario==null){
+            //no se encontro el ususario
+            throw new UsernameNotFoundException(username);
+        }
+        
+        //si estamos aca entonces si se encontro el usuario entonces se carga la foto
+        session.removeAttribute("imagenUsuario");
+        session.setAttribute("imagenUsuario", usuario.getRutaImagen());
+        
+        //se deben crear los rolesque vienen desde la tabla rol
+        var roles = new ArrayList<GrantedAuthority>();
+        
+        //se recorren los roles del usuario y se pasan al arreglo.. ya como rol de seguridad
+        for(Rol r : usuario.getRoles()){
+            roles.add(new SimpleGrantedAuthority(r.getNombre()));
+        }
+        
+        //se returna un usuario del sistema con username, password y roles
+        return new User(usuario.getUsername(),usuario.getPassword(),roles);
+        
+    }
+    
     
 }
