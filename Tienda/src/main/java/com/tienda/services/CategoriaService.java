c1d928cda3a422885051bbd76527578eaa4d6ff5
Alejandro1103 <113400144+Alejandro1103@users.noreply.github.com>
2024-08-16 00:10:32 -0600

    Commit 7

--- Tienda/src/main/java/com/tienda/services/CategoriaService.java
+++ Tienda/src/main/java/com/tienda/services/CategoriaService.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.tienda.services;
 
-/**
- *
- * @author Alejandro
- */
+import com.tienda.domain.Categoria;
+import java.util.List;
+
 public interface CategoriaService {
     
+    //Se obtiene los registros de la tabla categoria en un array list de objetos categoria, todos o solo los activos
+    public List<Categoria> getCategorias(boolean activos);
+    
+    //Recupera el registro de la tabla categoria en un objeto categoria si el idCategoria existe sino devuelve null
+    public Categoria getCategoria(Categoria categoria);
+    
+    //Actualiza un registro en la tabla categoria si el idCategoria existe
+    //crea un registro en la tabla categoria si idCategoria no tiene valor 
+    
+    public void save(Categoria categoria);
+    
+    //Elimina el registro de la tabla categoria si idCategoria existe en la tabla
+    public void delete(Categoria categoria);
 }
--- Tienda/src/main/java/com/tienda/services/FirebaseStorageService.java
+++ Tienda/src/main/java/com/tienda/services/FirebaseStorageService.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.tienda.services;
 
-/**
- *
- * @author Alejandro
- */
+import org.springframework.web.multipart.MultipartFile;
+
 public interface FirebaseStorageService {
-    
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-8f0d8.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-8f0d8-firebase-adminsdk-kq76w-3641721fb4.json";
 }
