4d6b52db4082425b03994be06006c7a786cda36b
Alejandro1103 <113400144+Alejandro1103@users.noreply.github.com>
2024-08-16 00:03:38 -0600

    Commit 4

--- Tienda/src/main/java/com/tienda/dao/ProductoDao.java
+++ Tienda/src/main/java/com/tienda/dao/ProductoDao.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.tienda.dao;
 
-/**
- *
- * @author Alejandro
- */
-public interface ProductoDao {
+import com.tienda.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ProductoDao extends JpaRepository<Producto,Long>{
     
+    //Una consulta ampliada por precio
+    public List<Producto> findByPrecioBetweenOrderByDescripcion (double precioInf, double precioSup); //introductores son maneras de filtrar lo que quiero buscar
+    
+    //una consulta mediante JPQL
+    @Query(value="SELECT a FROM Producto a WHERE a.precio BETWEEN :precioInf AND :precioSup ORDER BY a.descripcion ASC")
+    public List<Producto> consultaJPQL (double precioInf, double precioSup);
+    
+    //una consulta mediante SQL
+    @Query(nativeQuery=true, value="SELECT * FROM producto a WHERE a.precio BETWEEN :precioInf AND :precioSup ORDER BY a.descripcion ASC")
+    public List<Producto> consultaSQL (double precioInf, double precioSup);
 }
--- Tienda/src/main/java/com/tienda/dao/UsuarioDao.java
+++ Tienda/src/main/java/com/tienda/dao/UsuarioDao.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.tienda.dao;
 
-/**
- *
- * @author Alejandro
- */
-public interface UsuarioDao {
+import com.tienda.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioDao extends JpaRepository<Usuario,Long>{
     
+    public Usuario findByUsername(String username);
 }
